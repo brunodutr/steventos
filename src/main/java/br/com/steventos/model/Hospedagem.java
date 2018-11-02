19ace045a2575b8a5af0b1c0bc7d9bdac234d1f7
Bruno Dutra <brun_dutra@hotmail.com>
2018-11-02 16:27:38 -0300

    Criação dos Models Transporte e Hospedagem

---
+++ src/main/java/br/com/steventos/model/Hospedagem.java
@@ -0,0 +1,60 @@
+package br.com.steventos.model;
+
+import static javax.persistence.CascadeType.ALL;
+import static javax.persistence.GenerationType.AUTO;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+@Entity
+@Table(name = "HOSPEDAGEM")
+public class Hospedagem implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = AUTO)
+	private Long id;
+
+	private String nome;
+	
+	@JsonIgnore
+	@ManyToMany(mappedBy = "eventos", cascade = ALL, targetEntity = Pessoa.class)
+	private Set<Pessoa> pessoas;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Set<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(Set<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+}
+
