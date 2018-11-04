49952cb7053505d79dc04eabc6d54a1f225abbab
Bruno Dutra <brun_dutra@hotmail.com>
2018-11-03 22:26:39 -0300

    Alteração do componente <cidades-autocomplete>

--- src/main/angular/src/app/eventos/eventos-list/eventos-list.component.ts
+++ src/main/angular/src/app/eventos/eventos-list/eventos-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { EventoService } from "../eventos.service";
+import { CidadeService } from "src/app/shared/service/cidades.service";
 import { Evento } from "../evento.model";
+import { EventoService } from "../eventos.service";
+import { Local } from "src/app/shared/model/local.model";
 
 @Component({
   selector: "app-eventos-list",
@@ -9,14 +11,18 @@ import { Evento } from "../evento.model";
 })
 export class EventosListComponent implements OnInit {
   eventos: Evento[] = [];
+  local: Local;
 
-  constructor(private service: EventoService) {}
+  constructor(
+    private service: EventoService,
+    private cService: CidadeService
+  ) {}
 
   async ngOnInit() {
     this.eventos = await this.service.getAll();
   }
 
-  private cidadesValue(event) {
-    console.log(event);
+  private setCidade(event: Local) {
+    this.local = event;
   }
 }
